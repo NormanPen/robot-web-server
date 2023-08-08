c5654449e02c4056b6a5e0fd62e2a9cf79c4fd96
Norman Pendzich <Norman.Pendzich@damago.eu>
2023-08-08 09:51:05 +0200

    läuft

--- go.mod
+++ go.mod
@@ -1,3 +1,12 @@
 module github.com/NormanPen/robot-web-server
 
 go 1.20
+
+require github.com/rs/cors v1.9.0
+
+require (
+	github.com/yuin/goldmark v1.4.13 // indirect
+	golang.org/x/mod v0.12.0 // indirect
+	golang.org/x/sys v0.11.0 // indirect
+	golang.org/x/tools v0.12.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,8 +2,45 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+
+	"github.com/rs/cors"
 )
 
+func handleAction(w http.ResponseWriter, r *http.Request) {
+	action := r.URL.Path[len("/"):]
+
+	switch action {
+	case "start_motor":
+		fmt.Println("Motor gestartet")
+		w.Write([]byte("Motor gestartet"))
+	case "forward":
+		fmt.Println("Vorwärts")
+		w.Write([]byte("Vorwärts"))
+	case "backward":
+		fmt.Println("Rückwärts")
+		w.Write([]byte("Rückwärts"))
+	case "left":
+		fmt.Println("Links")
+		w.Write([]byte("Links"))
+	case "right":
+		fmt.Println("Rechts")
+		w.Write([]byte("Rechts"))
+	case "stop":
+		fmt.Println("Stopp")
+		w.Write([]byte("Stopp"))
+	default:
+		http.NotFound(w, r)
+		return
+	}
+}
+
 func main() {
-		fmt.Println("Hello World")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", handleAction)
+
+	corsHandler := cors.Default().Handler(mux)
+
+	fmt.Println("Die REST-API ist gestartet und hört auf Port 8080")
+	http.ListenAndServe(":8000", corsHandler)
 }
